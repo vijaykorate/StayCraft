7ee1922fb153efcdafdfd13ede4c494e47b15e87
Vijay Korate <vijaykorate18@gmail.com>
2026-02-13 18:19:46 +0530

    working on React Hook Form Library creating new cabin

---
+++ src/services/apiCabins.js
@@ -0,0 +1,32 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+  const { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("cabins could not be loaded");
+  }
+  return data;
+}
+
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase.from("cabins").insert([newCabin]);
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be Created");
+  }
+
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin couldnot be deleted");
+  }
+
+  return data;
+}
